11af32e667b1bcb670c3879878f742df6126492c
Mikhail Surin <antimony@yandex.ru>
2019-10-08 09:15:58 +0300

    Remove comments

--- SharpDummy.Web.Core/Attributes/AuthorizeAttribute.cs
+++ SharpDummy.Web.Core/Attributes/AuthorizeAttribute.cs
@@ -12,14 +12,12 @@ namespace SharpDummy.Web.Core.Attributes
         }
     }
 
-    // New attribute that is only usable at the class (controller) level
     [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
     public class ControllerAuthorizeAttribute : AuthorizeAttribute
     {
 
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
-            //If the target action has been decorated with an ActionAuthorizeAttribute, skip controller auth
             if (filterContext.ActionDescriptor.GetCustomAttributes(true).Any(a => a is NonAuthorizedAttribute))
             {
                 return;
